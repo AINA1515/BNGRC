74ab67ecb006af195170e23fa3b688af68b0819b
Bryan <bryanmihajatiana101@gmail.com>
2026-02-16 18:24:28 +0300

    Update VueDonsParVilleController.php

--- app/controllers/VueDonsParVilleController.php
+++ app/controllers/VueDonsParVilleController.php
@@ -23,4 +23,35 @@ class VueDonsParVilleController
             return ['status' => 'error', 'message' => 'Not found'];
         }
     }
+
+    public function getDonParVille($idVille){
+        $view = VueDonsParVilleModel::getDonParVille($idVille);
+        if ($view) {
+            return $view;
+        } else {
+            return ['status' => 'error', 'message' => 'Not found'];
+        }
+    }
+
+    public function countDonparType($idVille){
+        $view = VueDonsParVilleModel::countDonparType($idVille);
+        return $view;
+    }
+    public function getByIdVilleAndTypeDon($idVille,$typeDon){
+        $view = VueDonsParVilleModel::getByIdVilleAndTypeDon($idVille,$typeDon);
+        if ($view) {
+            return $view;
+        } else {
+            return ['status' => 'error', 'message' => 'Not found'];
+        }
+    }
+
+    public function getByIdVilleAndNomDon($idDon,$nomDon){
+        $view = VueDonsParVilleModel::getByIdVilleAndNomDon($idDon,$nomDon);
+        if ($view) {
+            return $view;
+        } else {
+            return ['status' => 'error', 'message' => 'Not found'];
+        }
+    }
 }
