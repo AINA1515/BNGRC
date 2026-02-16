47cc60687859e23ad4d884685254221a07f29f87
BlackS717 <atianiaina@gmail.com>
2026-02-16 17:08:47 +0300

    pass besoin ville in the router

--- app/config/routes.php
+++ app/config/routes.php
@@ -14,13 +14,28 @@ use flight\net\Router;
 $router->group('', function(Router $router) use ($app) {
 
 	$router->get('/', function() use ($app) {
-		$customChartData = [
-			'labels' => ['Ville 1', 'Ville 2', 'Ville 3', 'Ville 4 ', 'Ville 5', 'Ville 6'],
-			'data' => [1, 30, 5, 12, 10, 35]
+		$labels = ['Ville 1', 'Ville 2', 'Ville 3', 'Ville 4 ', 'Ville 5', 'Ville 6'];
+        $chartData = [1, 30, 5, 12, 10, 35];
+
+        $customChartData = [
+			'labels' => $labels,
+			'data' => $chartData
 		];
+
+        $besoinVilles = [
+            ["ville"=>"Ville 1 ", "besoin" => "Riz", "quantite" => "10kg", "pu" => "50000f"],
+            ["ville"=>"Ville 2 ", "besoin" => "Haricot", "quantite" => "20kg", "pu" => "30000f"],
+            ["ville"=>"Ville 3 ", "besoin" => "Maïs", "quantite" => "15kg", "pu" => "40000f"],
+            ["ville"=>"Ville 4 ", "besoin" => "Blé", "quantite" => "25kg", "pu" => "45000f"],
+            ["ville"=>"Ville 5 ", "besoin" => "Sorgho", "quantite" => "30kg", "pu" => "35000f"],
+            ["ville"=>"Ville 6 ", "besoin" => "Millet", "quantite" => "12kg", "pu" => "20000f"]
+        ];
+
+
 		$app->render('dashboard', [
 			'csp_nonce' => $app->get('csp_nonce'),
-			'sinistreChartData' => $customChartData
+			'sinistreChartData' => $customChartData,
+            'besoinVilles' => $besoinVilles
 		]);
 	});
 
